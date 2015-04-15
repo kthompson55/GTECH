22fcff921d24fda1c40907742de5e5bb762f4281
Devon Truman <dltruman2012@gmail.com>
2015-04-15 14:52:18 -0600

    PrizeLevel and PrizeLevels

---
+++ Collection Game Tool/Collection Game Tool/PrizeLevels/PrizeLevels.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection_Game_Tool.PrizeLevels
+{
+    class PrizeLevels
+    {
+        private List<PrizeLevel> prizeLevels = new List<PrizeLevel>();
+
+        public PrizeLevel getPrizeLevel(int index)
+        {
+            return prizeLevels.ElementAt(index);
+        }
+
+        public void addPrizeLevel(PrizeLevel obj)
+        {
+            prizeLevels.Add(obj);
+        }
+
+        public void removePrizeLevel(int index)
+        {
+            prizeLevels.RemoveAt(index);
+        }
+
+        public void addPrizeLevelAt(PrizeLevel obj, int index)
+        {
+            prizeLevels.Insert(index, obj);
+        }
+    }
+}

de6af0809d73145f928ddbc81d73f0f285ec5216
Devon Truman <dltruman2012@gmail.com>
2015-04-16 14:18:19 -0600

    PrizeLevel Tests

---
+++ Collection Game Tool/Collection Game Tool Test/PrizeLevelsTests/PrizeLevelTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Collection_Game_Tool.PrizeLevels;
+
+namespace Collection_Game_Tool_Test.PrizeLevelsTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Test_Prize_Comparison()
+        {
+            PrizeLevel pl1 = new PrizeLevel();
+            PrizeLevel pl2 = new PrizeLevel();
+
+            pl1.prizeValue = 50;
+            pl2.prizeValue = 100;
+
+            Assert.IsTrue(pl1.CompareTo(pl2) < 0);
+        }
+
+        [TestMethod]
+        public void Test_InstantWin_Toggle()
+        {
+            PrizeLevel pl1 = new PrizeLevel();
+
+            pl1.isInstantWin = false;
+
+            pl1.toggleIsInstantWin();
+
+            Assert.IsTrue(pl1.isInstantWin);
+
+            pl1.toggleIsInstantWin();
+
+            Assert.IsFalse(pl1.isInstantWin);
+        }
+    }
+}

61e528a52cb630a0f866e9ac9e376d3f9e313687
Devon Truman <dltruman2012@gmail.com>
2015-04-22 14:02:27 -0600

    Update to Devon's Work

--- Collection Game Tool/Collection Game Tool/PrizeLevels/PrizeLevel.cs
+++ Collection Game Tool/Collection Game Tool/PrizeLevels/PrizeLevel.cs
@@ -1,14 +1,52 @@
-﻿using System;
+﻿using Collection_Game_Tool.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Collection_Game_Tool.PrizeLevels
 {
-    public class PrizeLevel : IComparable
+    public class PrizeLevel : IComparable, Teller
     {
-        public double prizeValue{ get; set; }
+        List<Listener> audience =new List<Listener>();
+        static List<String> levels = new List<String>()
+        {
+            "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T"
+        };
+
+        private int pl;
+        public int prizeLevel
+        {
+            get
+            {
+                return pl;
+            }
+            set
+            {
+                pl = value;
+                shout("Level");
+            }
+        }
+
+        private double pv;
+        public double prizeValue
+        {
+            get
+            {
+                return pv;
+            }
+            set
+            {
+                pv = value;
+            }
+        }
+
+        public String currentLevel()
+        {
+            return levels[prizeLevel-1];
+        }
 
         public int numCollections{ get; set; }
 
@@ -23,6 +61,19 @@ namespace Collection_Game_Tool.PrizeLevels
         public void toggleIsInstantWin()
         {
             isInstantWin = !isInstantWin;
+        }
+
+        public void shout(object pass)
+        {
+            foreach (Listener fans in audience)
+            {
+                fans.onListen(pass);
+            }
+        }
+
+        public void addListener(Listener list)
+        {
+            audience.Add(list);
         }
     }
 }
---
+++ Collection Game Tool/Collection Game Tool/PrizeLevels/UserControlPrizeLevels.xaml.cs
@@ -0,0 +1,96 @@
+﻿using Collection_Game_Tool.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Collection_Game_Tool.PrizeLevels
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class UserControlPrizeLevels : UserControl, Listener
+    {
+        PrizeLevels plsObject;
+
+        public UserControlPrizeLevels()
+        {
+            InitializeComponent();
+            plsObject = new PrizeLevels();
+
+            UserControlPrizeLevel ucpl = new UserControlPrizeLevel();
+            ucpl.addListener(this);
+            Prizes.Children.Add(ucpl);
+            plsObject.addPrizeLevel(ucpl.plObject);
+            ucpl.plObject.prizeLevel=1;
+        }
+
+        public void Add_Prize_Level(object sender, RoutedEventArgs e)
+        {
+            UserControlPrizeLevel ucpl = new UserControlPrizeLevel();
+            ucpl.Margin = new Thickness(0,Prizes.Children.Count * 90,0,0);
+
+            ucpl.addListener(this);
+            Prizes.Children.Add(ucpl);
+            plsObject.addPrizeLevel(ucpl.plObject);
+            ucpl.plObject.prizeLevel = Prizes.Children.Count;
+        }
+
+        public void onListen(object pass)
+        {
+            if (pass is string)
+            {
+                String parse=(String)pass;
+                if (parse.Equals("Value"))
+                {
+                    List<UserControlPrizeLevel> ucplList = new List<UserControlPrizeLevel>();
+                    ucplList = Prizes.Children.Cast<UserControlPrizeLevel>().ToList<UserControlPrizeLevel>();
+                    Prizes.Children.Clear();
+
+                    ucplList.Sort();
+
+                    for (int i = 0; i < ucplList.Count; i++ )
+                    {
+                        ucplList[i].Margin = new Thickness(0, i*90, 0, 0);
+                        ucplList[i].plObject.prizeLevel = (i + 1);
+                        Prizes.Children.Add(ucplList[i]);
+                    }
+                }
+            }
+            else
+            {
+                UserControlPrizeLevel rem = (UserControlPrizeLevel)pass;
+
+                int index = -1;
+                for (int i = 0; i < Prizes.Children.Count && index < 0; i++)
+                {
+                    UserControlPrizeLevel ucpl = (UserControlPrizeLevel)Prizes.Children[i];
+                    if (ucpl == rem)
+                        index = i;
+                }
+
+                Prizes.Children.Remove(rem);
+                plsObject.removePrizeLevel(index);
+
+                rem = null;
+
+                for (int i = 0; i < Prizes.Children.Count; i++)
+                {
+                    UserControlPrizeLevel ucpl = (UserControlPrizeLevel)Prizes.Children[i];
+                    ucpl.Margin = new Thickness(0, i * 90, 0, 0);
+                    ucpl.plObject.prizeLevel = (i + 1);
+                }
+            }
+        }
+    }
+}

7eda5dd0bf985c8e00071c6e5beef9fc3c7b3f17
Luis Andrade <landrade1093@gmail.com>
2015-04-29 14:34:13 -0600

    Forgot to commit new PrizeLevelBox user control

---
+++ Collection Game Tool/Collection Game Tool/Divisions/PrizeLevelBox.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+
+namespace Collection_Game_Tool.Divisions
+{
+    /// <summary>
+    /// Interaction logic for PrizeLevelBox.xaml
+    /// </summary>
+    public partial class PrizeLevelBox : UserControl, INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DivisionUC division;
+        public bool IsSelected { get; set; }
+
+        private bool _isAvailable;
+        public bool IsAvailable
+        {
+            get
+            {
+                return _isAvailable;
+            }
+            set
+            {
+                _isAvailable = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("IsAvailable"));
+            }
+        }
+
+        private int _prizeBoxLevel;
+        public int PrizeBoxLevel 
+        {
+            get
+            {
+                return _prizeBoxLevel;
+            }
+            set
+            {
+                _prizeBoxLevel = value;
+
+                if (PropertyChanged != null)
+                    PropertyChanged(this, new PropertyChangedEventArgs("PrizeBoxLevel"));
+            }
+        }
+
+        public PrizeLevelBox(DivisionUC div, bool available, int level)
+        {
+            InitializeComponent();
+            levelBox.DataContext = this;
+            prizeLevelLabel.DataContext = this;
+
+            this.division = div;
+            IsSelected = false;
+            IsAvailable = available;
+            PrizeBoxLevel = level;
+        }
+
+        private void levelBox_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (IsSelected)
+            {
+                levelBox.Background = Brushes.White;
+            }
+            else
+            {
+                levelBox.Background = new SolidColorBrush(Color.FromRgb(0, 157, 217));
+            }
+            IsSelected = !IsSelected;
+        }
+
+    }
+}
